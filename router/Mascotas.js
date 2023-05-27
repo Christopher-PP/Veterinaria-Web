2406eea2d8b30a0c3c17d7219f542c50a624e77d
Christopher <alucart9@gmail.com>
2023-05-27 09:57:09 -0700

    Practica terminada

--- router/Mascotas.js
+++ router/Mascotas.js
@@ -31,4 +31,58 @@ router.post("/crear", async (req, res) => {
     console.log(error);
   }
 });
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const mascotasdb = await mascota.findOne({ _id: id });
+    if (!mascotasdb) {
+      return res.render("detalle", {
+        error: true,
+        adv_messaje: "mascota no encontrada",
+      });
+    }
+    return res.render("detalle", { error: false, mascotasdb });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const mascotaDB = await mascota.findByIdAndDelete({ _id: id });
+    if (!mascotaDB) {
+      return res.json({
+        estado: true,
+        message: "Fallo al eliminar!",
+      });
+    }
+    return res.json({
+      estado: true,
+      message: "Registro Eliminado!",
+    });
+  } catch (error) {}
+});
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  try {
+    const mascotaDB = await mascota.findByIdAndUpdate(id, body, {
+      useFindAndModify: false,
+    });
+    if (!mascotaDB) {
+      return res.json({
+        estado: false,
+        message: "Fallo la actualizacion!",
+      });
+    }
+    return res.json({
+      estado: true,
+      message: "Actualizado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 module.exports = router;
